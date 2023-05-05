953263ef30b7cbadcd4c260187af7e0b5272c159
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-05-05 14:38:01 +0500

    map

--- scripts/script.js
+++ scripts/script.js
@@ -192,6 +192,71 @@ function applyBtn() {
   console.log('ok')
 }
 
+// закрытие окна с фильтром
+document.querySelector('.close_btn').addEventListener('click', () => {
+  document.querySelector('.filter_screen').classList.remove('active_filter');
+  document.querySelector('.map').classList.add('active_filter');
+});
+
+// открытие окна с фильтром
+document.querySelector('.burger').addEventListener('click', () => {
+  document.querySelector('.filter_screen').classList.add('active_filter');
+  document.querySelector('.map').classList.remove('active_filter');
+});
+
+
+/// map
+
+function init() {
+  // Задаём точки мультимаршрута.
+  var pointA = [55.749, 37.524],
+      pointB = "Москва, Красная площадь",
+      /**
+       * Создаем мультимаршрут.
+       * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+       */
+      multiRoute = new ymaps.multiRouter.MultiRoute({
+          referencePoints: [
+              pointA,
+              pointB
+          ],
+          params: {
+              //Тип маршрутизации - пешеходная маршрутизация.
+              routingMode: 'pedestrian'
+          }
+      }, {
+          // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+          boundsAutoApply: false
+      });
+
+  // Создаем кнопку.
+  var changePointsButton = new ymaps.control.Button({
+      data: {content: "Поменять местами точки А и В"},
+      options: {selectOnClick: true}
+  });
+
+  // Объявляем обработчики для кнопки.
+  changePointsButton.events.add('select', function () {
+      multiRoute.model.setReferencePoints([pointB, pointA]);
+  });
+
+  changePointsButton.events.add('deselect', function () {
+      multiRoute.model.setReferencePoints([pointA, pointB]);
+  });
+
+  // Создаем карту с добавленной на нее кнопкой.
+  var myMap = new ymaps.Map('map', {
+      center: [55.739625, 37.54120],
+      zoom: 16,
+      controls: []
+  });
+
+  // Добавляем мультимаршрут на карту.
+  myMap.geoObjects.add(multiRoute);
+}
+
+ymaps.ready(init);
+
 
 
 
