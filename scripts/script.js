f71e4c8a72bb9fc5fb73ee93b23f74ab08af0e57
Алексей Линьков <60229705+Dr-Alex-Dr@users.noreply.github.com>
2023-05-06 22:20:58 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -206,56 +206,12 @@ document.querySelector('.burger').addEventListener('click', () => {
 
 
 /// map
-
-function init() {
-  // Задаём точки мультимаршрута.
-  var pointA = [55.749, 37.524],
-      pointB = "Москва, Красная площадь",
-      /**
-       * Создаем мультимаршрут.
-       * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
-       */
-      multiRoute = new ymaps.multiRouter.MultiRoute({
-          referencePoints: [
-              pointA,
-              pointB
-          ],
-          params: {
-              //Тип маршрутизации - пешеходная маршрутизация.
-              routingMode: 'pedestrian'
-          }
-      }, {
-          // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-          boundsAutoApply: false
-      });
-
-  // Создаем кнопку.
-  var changePointsButton = new ymaps.control.Button({
-      data: {content: "Поменять местами точки А и В"},
-      options: {selectOnClick: true}
-  });
-
-  // Объявляем обработчики для кнопки.
-  changePointsButton.events.add('select', function () {
-      multiRoute.model.setReferencePoints([pointB, pointA]);
-  });
-
-  changePointsButton.events.add('deselect', function () {
-      multiRoute.model.setReferencePoints([pointA, pointB]);
-  });
-
-  // Создаем карту с добавленной на нее кнопкой.
-  var myMap = new ymaps.Map('map', {
-      center: [55.739625, 37.54120],
-      zoom: 16,
-      controls: []
-  });
-
-  // Добавляем мультимаршрут на карту.
-  myMap.geoObjects.add(multiRoute);
-}
-
-ymaps.ready(init);
+const map = new ymaps3.YMap(document.getElementById('YMapsID'), {
+  location: {
+    center: [37.64, 55.76],
+    zoom: 10
+  }
+});
 
 
 
