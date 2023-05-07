fdd6432012837f8c0eef70836d21f431821f5e62
Dr-Alex-Dr <mr.lopin900@gmail.com>
2023-05-07 11:45:11 +0500

    router

--- scripts/script.js
+++ scripts/script.js
@@ -189,7 +189,8 @@ function unActiveApplybtn() {
 }
 
 function applyBtn() {
-  console.log('ok')
+  document.querySelector('.filter_screen').classList.remove('active_filter');
+  document.querySelector('.map').classList.add('active_filter');
 }
 
 // закрытие окна с фильтром
@@ -201,135 +202,60 @@ document.querySelector('.close_btn').addEventListener('click', () => {
 // открытие окна с фильтром
 document.querySelector('.burger').addEventListener('click', () => {
   document.querySelector('.filter_screen').classList.add('active_filter');
-  document.querySelector('.map').classList.add('active_filter');
+  document.querySelector('.map').classList.remove('active_filter');
 });
 
 
 /// map
 function init() {
-  // Объявляем набор опорных точек и массив индексов транзитных точек.
-  var referencePoints = [
-          "Москва, Ленинский проспект",
-          "Москва, Льва Толстого, 16",
-          "Москва, Кремлевская набережная",
-          "Москва, парк Сокольники"
-      ],
-      viaIndexes = [2];
-
-  // Создаем мультимаршрут и настраиваем его внешний вид с помощью опций.
-  var multiRoute = new ymaps.multiRouter.MultiRoute({
-      referencePoints: referencePoints,
-      params: {viaIndexes: viaIndexes}
+  var points = [
+    { name: "Храм на крови", latitude: 56.844402, longitude: 60.609081 },
+    { name: "Орден Ленина", latitude: 56.856638, longitude: 60.60376 },
+    { name: "Жертвам репрессий", latitude: 56.827695, longitude: 60.579881 }
+  ];
+    // Создаем карту с добавленной на нее кнопкой.
+    var map = new ymaps.Map('map', {
+      center: [56.8519, 60.6122],
+      zoom: 11,
+      controls: []
   }, {
-      // Внешний вид путевых точек.
-      wayPointStartIconColor: "#333",
-      wayPointStartIconFillColor: "#B3B3B3",
-      // Задаем собственную картинку для последней путевой точки.
-      wayPointFinishIconLayout: "default#image",
-      wayPointFinishIconImageHref: "images/sokolniki.png",
-      wayPointFinishIconImageSize: [30, 30],
-      wayPointFinishIconImageOffset: [-15, -15],
-      // Позволяет скрыть иконки путевых точек маршрута.
-      // wayPointVisible:false,
-
-      // Внешний вид транзитных точек.
-      viaPointIconRadius: 7,
-      viaPointIconFillColor: "#000088",
-      viaPointActiveIconFillColor: "#E63E92",
-      // Транзитные точки можно перетаскивать, при этом
-      // маршрут будет перестраиваться.
-      viaPointDraggable: true,
-      // Позволяет скрыть иконки транзитных точек маршрута.
-      // viaPointVisible:false,
-
-      // Внешний вид точечных маркеров под путевыми точками.
-      pinIconFillColor: "#000088",
-      pinActiveIconFillColor: "#B3B3B3",
-      // Позволяет скрыть точечные маркеры путевых точек.
-      // pinVisible:false,
-
-      // Внешний вид линии маршрута.
-      routeStrokeWidth: 2,
-      routeStrokeColor: "#000088",
-      routeActiveStrokeWidth: 6,
-      routeActiveStrokeColor: "#E63E92",
-
-      // Внешний вид линии пешеходного маршрута.
-      routeActivePedestrianSegmentStrokeStyle: "solid",
-      routeActivePedestrianSegmentStrokeColor: "#00CDCD",
-
-      // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
-      boundsAutoApply: true
+      buttonMaxWidth: 300
   });
+  
+  ymaps.geolocation.get({
+    provider: 'yandex',
+    mapStateAutoApply: true
+}).then(function (result) {
+  let p = result.geoObjects.position;
+  points.unshift({ name: "старт", latitude: p[0], longitude: p[1] })
+  console.log(points)
 
-  // Настраиваем внешний вид второй точки через прямой доступ к ней.
-  customizeSecondPoint();
+          // Создаем массив для хранения точек
+          var geoObjects = [];
 
-  // Создаем кнопки.
-  var removePointsButton = new ymaps.control.Button({
-          data: {content: "Удалить промежуточные точки"},
-          options: {selectOnClick: true}
-      }),
-      routingModeButton = new ymaps.control.Button({
-          data: {content: "Пешком"},
-          options: {selectOnClick: true}
-      });
-
-  // Объявляем обработчики для кнопок.
-  removePointsButton.events.add('select', function () {
-      multiRoute.model.setReferencePoints([
-          referencePoints[0],
-          referencePoints[referencePoints.length - 1]
-      ], []);
-  });
-
-  removePointsButton.events.add('deselect', function () {
-      multiRoute.model.setReferencePoints(referencePoints, viaIndexes);
-      // Т.к. вторая точка была удалена, нужно заново ее настроить.
-      customizeSecondPoint();
-  });
-
-  routingModeButton.events.add('select', function () {
-      multiRoute.model.setParams({routingMode: 'pedestrian'}, true);
-  });
-
-  routingModeButton.events.add('deselect', function () {
-      multiRoute.model.setParams({routingMode: 'auto'}, true);
-  });
-
-  // Функция настройки внешнего вида второй точки.
-  function customizeSecondPoint() {
-      /**
-       * Ждем, пока будут загружены данные мультимаршрута и созданы отображения путевых точек.
-       * @see https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRouteModel-docpage/#event-requestsuccess
-       */
-      multiRoute.model.events.once("requestsuccess", function () {
-          var yandexWayPoint = multiRoute.getWayPoints().get(1);
-          // Создаем балун у метки второй точки.
-          ymaps.geoObject.addon.balloon.get(yandexWayPoint);
-          yandexWayPoint.options.set({
-              preset: "islands#grayStretchyIcon",
-              iconContentLayout: ymaps.templateLayoutFactory.createClass(
-                  '<span style="color: red;">Я</span>ндекс'
-              ),
-              balloonContentLayout: ymaps.templateLayoutFactory.createClass(
-                  '{{ properties.address|raw }}'
-              )
+          // Добавляем точки на карту
+          for (var i = 0; i < points.length; i++) {
+            var point = points[i];
+            geoObjects[i] = new ymaps.Placemark([point.latitude, point.longitude], {
+              balloonContent: point.name
+            });
+          }
+        
+          // Создаем маршрут и добавляем его на карту
+          var route = new ymaps.multiRouter.MultiRoute({
+            referencePoints: geoObjects.map(function (geoObject) {
+              return geoObject.geometry.getCoordinates();
+            }),
+            params: {
+              results: 1 // ограничиваем количество альтернативных маршрутов до одного
+            }
+          }, {
+            boundsAutoApply: true // автоматически подгоняем размер карты под маршрут
           });
-      });
-  }
-
-  // Создаем карту с добавленной на нее кнопкой.
-  var myMap = new ymaps.Map('map', {
-          center: [55.739625, 37.54120],
-          zoom: 7,
-          controls: [removePointsButton, routingModeButton]
-      }, {
-          buttonMaxWidth: 300
-      });
-
-  // Добавляем мультимаршрут на карту.
-  myMap.geoObjects.add(multiRoute);
+        
+          map.geoObjects.add(route); // добавляем маршрут на карту
+          map.geoObjects.add(geoObjects); // добавляем точки на карту
+  });
 }
 
 ymaps.ready(init);
