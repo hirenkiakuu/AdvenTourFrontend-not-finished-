99d7e1615daa14d5b36238e9a3a05c94fe245c62
Алексей Линьков <60229705+Dr-Alex-Dr@users.noreply.github.com>
2023-05-10 17:55:11 +0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-ymaps.ready(init);
+
 for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
     e.style.setProperty('--value', e.value);
     e.style.setProperty('--min', e.min == '' ? '0' : e.min);
@@ -257,7 +257,7 @@ map.geoObjects.add(route); // добавляем маршрут на карту
 map.geoObjects.add(geoObjects); // добавляем точки на карту
 }
 
-
+ymaps.ready(init);
 
 
 let paramPath = document.querySelector('.paramPath');
